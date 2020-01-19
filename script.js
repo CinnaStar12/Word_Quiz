c896ba0425c21f6f9b64bb5f7e6fca7f562aee05
Allan Binayao <allan.binayao@gmail.com>
2020-01-19 13:37:41 -0600

    Quiz functions and timer complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var start = $("#start-btn");
 var questionTitle = $("#questionDiv");
-var secondsLeft = 75;
+var secondsLeft = 0;
 var timer = $("#timer");
 var qNumber = 0;
 var message = $("<h2>");
@@ -8,15 +8,12 @@ var message = $("<h2>");
 
 function runQuiz(qNumber) {
     //find question title, choices, and answer
-    var choices = questions[qNumber].choices;
-    var answer = questions[qNumber].answer;
     var selection = $("<ol>");
     //write and append question
     questionTitle.text(questions[qNumber].title);
     questionTitle.append(selection);
     //create buttons and append for choices
-    console.log(answer);
-    choices.forEach(function (choice) {
+    questions[qNumber].choices.forEach(function (choice) {
         var choiceList = $("<li>")
         var choiceBtn = $("<button>");
         choiceBtn.text(choice)
@@ -29,36 +26,75 @@ function runQuiz(qNumber) {
     selection.append(message);
 
     $("button").on("click", function (event) {
-        if (event.target.textContent === answer) {
+        if (event.target.textContent === questions[qNumber].answer) {
             message.text("Correct!");
             qNumber++;
+            if(qNumber > 9)
+            {
+                var score = secondsLeft;
+                endQuiz();
+                clearInterval(timeLimit);
+                
+                
+            }
+            else{
             runQuiz(qNumber);
+            }
         }
         else {
             message.text("Wrong!");
-            secondsLeft -= 10;
+            secondsLeft -= 15;
+            
         }
     })
 
 }
-function timeLeft(){
-    var timeLimit = setInterval(function(){
+function timeLeft(time){
+        secondsLeft = time;
+        timeLimit = setInterval(function(){
         secondsLeft--;
         timer.text("Time: " + secondsLeft);
         if(secondsLeft <= 0)
         {
             endQuiz();
+            clearInterval(timeLimit);
         }
     }, 1000);
 }
 
-start.on("click", function () {
+start.on("click", startQuiz);
+
+function startQuiz(){
     start.remove();
-    timeLeft();
+    timeLeft(150);
     runQuiz(0);
 
-
-})
+}
 function endQuiz(){
     questionTitle.remove();
+    var endScreen = $("<div>");
+    endScreen.attr("class", "col-md text-center")
+    $("body").append(endScreen);
+    message.text("Quiz Over!");
+    endScreen.append(message);
+    var score = $("<div>");
+    finalScore = secondsLeft;
+    if(finalScore < 0){
+        finalScore = 0;
+    }
+    score.text("Your score is " + finalScore + "!")
+    message.append(score);
+    clearInterval();
+    highScoreBtn = $("<button>")
+    highScoreBtn.text("Save High Score")
+    highScoreBtn.attr("class", "btn btn-primary")
+    endScreen.append(highScoreBtn);
+}
+highScoreBtn.on("click", function(){
+    endScreen.remove();
+    input = $("<form>");
+
+})
+function saveHighScore(){
+
 }
